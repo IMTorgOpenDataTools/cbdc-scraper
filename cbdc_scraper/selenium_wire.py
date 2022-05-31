02ca751c8a1833b69d6124f1cc05e8204c99f199
IMTorg <information@mgmt-tech.org>
2022-05-31 15:31:52 -0400

    del unused functionality

--- cbdc_scraper/selenium_wire.py
+++ cbdc_scraper/selenium_wire.py
@@ -1,7 +1,7 @@
 
 
 
-from seleniumwire import webdriver  # Import from seleniumwire
+from seleniumwire import webdriver
 from seleniumwire.utils import decode
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -13,6 +13,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from pathlib import Path
 import time
 
+
+
 # Options
 chrome_options = Options()
 chrome_options.headless = True
@@ -22,88 +24,22 @@ chrome_options.add_argument('--ignore_http_methods')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--connection_keep_alive')
-#chrome_options.add_argument({'request_storage': 'memory'})
-#chrome_options.add_argument({'request_storage_max_size': '100'})
-
-# Enable Performance Logging of Chrome.
-desired_capabilities = DesiredCapabilities.CHROME
-desired_capabilities["goog:loggingPrefs"] = {"performance": "ALL"}
-desired_capabilities['acceptSslCerts'] = True
-desired_capabilities['acceptInsecureCerts'] = True
-
-def interceptor_body(request):
-    if 'doc' not in request.url:
-        if request.response:
-            if request.response.body:
-                request.response.body = ''
-
-def interceptor_abort(request, response):
-    if 'text/csv' in response.headers['Content-Type']:
-        response.headers['Target'] = True
-    else:
-        request.abort
 
 
 
 
 def selenium_wire_scrape():
-    #url = 'https://www.google.com'
-    #url = "https://www.atlanticcouncil.org/cbdctracker/" 
+    # setup
+    #url = "https://www.atlanticcouncil.org/cbdctracker/"                       #get svg url from iframe 
     url = "https://geoecon.github.io/21.10.20-CBDC-Tracker-Update-R1/?params="
     chrome_driver = "./.libs/chromedriver"
     path_driver = Path(chrome_driver).absolute().__str__()
-    driver = webdriver.Chrome(path_driver, chrome_options=chrome_options)    #, desired_capabilities=desired_capabilities)
-    driver.response_interceptor = interceptor_abort
-    # Create a new instance of the Chrome driver
+    driver = webdriver.Chrome(path_driver, chrome_options=chrome_options)
 
-    # Go to the Google home page
-    #driver.scopes = ['*google*',"*doc*","*geo*"]
+    # get url of googledocs data supplier
     driver.get(url)
     time.sleep(1)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    pat = 'doc-08-0k'
-    N = 6
-    
-    """
-    try:
-        elem = WebDriverWait(driver, N).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "svelte-1r1jv7l"))           # the svg map
-        )
-    except:
-        pass
-        #driver.quit()
+    urls = [req.url for req in driver.requests if 'docs.google.com' in req.url]
 
-    try:
-        request = driver.wait_for_request(pat, timeout = N)
-    except:
-        pass
-    """
-
-
-    # Access requests via the `requests` attribute
-    """
-    for request in driver.requests:
-        if request.response:
-            print(
-                request.url,
-                request.response.status_code,
-                request.response.headers['Content-Type']
-            )
-    """
-    response = driver.requests[0].response
-    #body = decode(response.body, response.headers.get('Content-Encoding', 'identity'))
-
-
-
-    responses = [request.response for request in driver.requests]
-    responses = [request.response for request in driver.requests]
-    entries = []
-    for resp in responses:
-        headers = [hdr for hdr in resp.headers if hdr[0] == 'content-type']
-        if headers:
-            content_hdrs = [hdr for hdr in headers if hdr[1]=='text/csv']
-            if content_hdrs:
-                entries.append(resp)
-
-
-    return True
+    return urls
--- tests/test_browser.py
+++ tests/test_browser.py
@@ -8,19 +8,9 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 
 
-from cbdc_scraper.selenium_bmop import selenium_scrape
 from cbdc_scraper.selenium_wire import selenium_wire_scrape
-from cbdc_scraper.puppet import puppet_scrape
 
-
-def test_selenium_scrape():
-    check = selenium_scrape()
-    assert True == True
 
 def test_selenium_wire_scrape():
-    check = selenium_wire_scrape()
-    assert True == True
-
-def test_puppet_scrape():
-    check = puppet_scrape()
-    assert True == True
+    url = selenium_wire_scrape()
+    assert url[0] == 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQh27kpYjCRmNoWa4FEpWqLSxLLaqK_hlgqP6wGQLp8Pum7guAYS6i0qt6wIRAPvb5Up6-6wvmTN05s/pub?gid=0&single=true&output=csv'
