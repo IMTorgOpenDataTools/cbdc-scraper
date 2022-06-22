c63431b470ff2f98c51f261a573678928962977f
IMTorg <information@mgmt-tech.org>
2022-06-22 15:16:17 -0400

    mod to format both scraped sites' data for validation.  stop for analysis.

--- cbdc_scraper/utils.py
+++ cbdc_scraper/utils.py
@@ -42,12 +42,17 @@ def get_data_cbdc():
             "columns":"currencies/columns",
             "history":"history-of-changes?page=0&size=1"
             }
-    
     headers = {
         "User-Agent": "Test User",
         "Accept-Encoding": "gzip, deflate",
         "Host": "efts.sec.gov"
         }
+    columns = {"country":"Country", "status":"Status", "Changed Status":"Changed Status", "valueOld":"Last Qtr Status",
+                "centralBank":"Central Bank", 
+                "digitalCurrency":"Digital Currency Name", "type":"Purpose",
+                'technology':"Technology provider", "technologyName":"Software",
+                'dlt':"Ledger Type", 'Permission':"Permission"
+    }
 
     #request data
     data = {}
@@ -68,9 +73,45 @@ def get_data_cbdc():
     df_currencies = pd.DataFrame(data["currencies"])
     df_currencies.sort_values(by=['country','digitalCurrency'], inplace=True)                      #<<<SELECTION_CHOICE
     df_currencies.drop_duplicates(subset=["country"], inplace=True)
-    data["currencies"] = df_currencies.to_dict("records")
-    
-    return data
+
+    # additional tables
+    df_tags = pd.DataFrame(data['tags'])
+    changed = []
+    content = data["history"]["content"][0]
+    for item in df_tags.to_dict('records'):
+        step1 = [hist for hist in content["tags"] 
+                    if (hist["tag"]["name"]==item["name"])
+                    ]
+        changes = step1[0]["changes"] if len(step1)>0 else []
+        change_list = [chg for chg in changes if chg["property"]=="status"]
+        if change_list != []:
+            change_list[0]['name'] = item['name']
+            changed.extend(change_list)
+        else:
+            continue
+    df_changed = pd.DataFrame(changed)
+
+    # merge tables
+    df1 = pd.merge(df_currencies, df_tags, 
+            left_on="tag", right_on="name",
+            suffixes=["","tags"],
+            how = "left"
+            )
+    df2 = pd.merge(df1, df_changed[['name','valueOld','valueNew']], 
+            left_on="tag", right_on="name",
+            suffixes=["","chgd"],
+            how = "left"
+            )
+    df2["Changed Status"] = np.nan
+    df2.loc[ ~df2["valueNew"].isnull(), "Changed Status"] = "Yes"
+    df2['Permission'] = np.nan
+
+    df2.rename(columns=columns, inplace=True)
+    df3 = df2[ list(columns.values()) ]
+
+    data_dict = {}
+    data_dict["cbdc"] = df3.to_dict("records")
+    return data_dict
 
 
 
@@ -80,20 +121,37 @@ def get_data_atlantic():
     """
     url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQh27kpYjCRmNoWa4FEpWqLSxLLaqK_hlgqP6wGQLp8Pum7guAYS6i0qt6wIRAPvb5Up6-6wvmTN05s/pub?gid=0&single=true&output=csv"
     files = {"Atlantic": "CBDC Tracker Current Updates"}
-    columns = ["Name", "Present Status", "Underlying technology"]            #TODO: add as primary Status and change from prev qtr status
-    prev_qtr_month = {1:"February Status", 2:"April Status", 3:"June Status", 4:"October Status"}
+
+    qtr_month = {4:"February Status", 1:"April Status", 2:"June Status", 3:"October Status"}
+
+    columns = {"Name":"Country", "Central Bank Name":"Central Bank", 
+                "Name of CBDC":"Digital Currency Name", "Use case":"Purpose",
+                'Technology partnership':"Technology provider", "Underlying technology":"Software",
+                'Infrastructure: DLT or conventional':"Ledger Type", 'Access: token or account':"Permission"
+    }
+    
+    def get_prev_qtr(current_qtr):
+        match current_qtr:
+            case 2 | 3 | 4: 
+                prev_qtr = current_qtr - 1
+            case 1: 
+                prev_qtr = 4
+        return prev_qtr
+
 
     data_dict = {}
     try:
         loaded_df = pd.read_csv(url)
 
         current_qtr = pd.Timestamp(dt.date.today()).quarter
-        columns.append( prev_qtr_month[current_qtr] )
-        df = loaded_df[columns]
-        column_replacements = {v:'Previous Status' for k,v in prev_qtr_month.items()}
-        df.rename(columns=column_replacements, inplace=True)
+        current_qtr_month = qtr_month[current_qtr]
+        columns[current_qtr_month] = "Status"
+        last_qtr_month = qtr_month[ get_prev_qtr(current_qtr) ]
+        columns[last_qtr_month] = "Last Qtr Status"
+        loaded_df.rename(columns=columns, inplace=True)
+        df = loaded_df[ list(columns.values()) ]
         df["Changed Status"] = "No"
-        df.loc[ df["Previous Status"] != df["Present Status"], "Changed Status"] = "Yes"
+        df.loc[ df["Last Qtr Status"] != df["Status"], "Changed Status"] = "Yes"
 
         k,v = list(files.items())[0]
         data_dict[k] = df.to_dict("records")
@@ -107,22 +165,31 @@ def get_data_atlantic():
 
 
 
-def check_dataframe(df):
-    """Check dataframe for basic problems"""
-    columns = ["country", "mod_status", "centralBank", "tag", 
-                "type", "technology", "Underlying technology", "dlt", 
-                "description", "Changed Status", "Previous Status"
-                ]
-    idx_cols = ['country','mod_status']
+def check_dataframes(data_dict):
+    """Check both dataframes for basic problems, and compare for validity.
+    """
 
-    cols = all( [col in df.columns for col in columns] )
-    obs = df.shape[0] > 0
-    dups = df[df.duplicated(subset=idx_cols) == True].shape[0] == 0
-    missing = df[df[idx_cols].isnull().any(axis=1)].shape[0] == 0  if obs == True else False
+    columns = ['Country', 'Status', 'Changed Status', 'Last Qtr Status', 
+                'Central Bank', 'Digital Currency Name', 'Purpose', 
+                'Technology provider', 'Software', 'Ledger Type', 'Permission']
+    idx_cols = ['Country','Status']
 
-    checks = [cols, obs, dups, missing]
-    check = all(checks) == True
-    return check
+    result = []
+    for key, recs in data_dict.items():
+        df = pd.DataFrame(recs)
+        try:
+            cols = all( [col in df.columns for col in columns] )
+            obs = df.shape[0] > 0
+            dups = df[df.duplicated(subset=idx_cols) == True].shape[0] == 0
+            missing = df[df[idx_cols].isnull().any(axis=1)].shape[0] == 0  if obs == True else False
+        except:
+            logger.error("error in checking dfs")
+            exit()
+
+        checks = [cols, obs, dups, missing]
+        check = all(checks) == True
+        result.append(check)
+    return result
 
 
 
@@ -132,49 +199,50 @@ def process_data(data_dict):
     input: dict_of_records
     output: recs
     """
-    recs = []
-    df_currencies = pd.DataFrame(data_dict["currencies"])
-    df_atlantic = pd.DataFrame(data_dict["Atlantic"])
-    
-    df = pd.merge(df_currencies, df_atlantic, 
-            left_on="country", right_on="Name",
-            suffixes=["","atlantic"],
-            how = "left"
-            )
-    df["mod_status"] = df["Present Status"]
-    df['mod_status'][df["mod_status"].isna()==True] = df['status']                      #<<<SELECTION_CHOICE
 
-    check = check_dataframe(df)
+    check = check_dataframes(data_dict)
     if not check:
         logger.error("dataframe did not meet `check_dataframe()` requirements")
         output.send_notification(error=True)
         exit()
-    data_dict["merged"] = df.to_dict("records")
 
+    df_cbdc = pd.DataFrame(data_dict["cbdc"])
+    df_atlantic = pd.DataFrame(data_dict["Atlantic"])  
+    df = df_atlantic  
+    '''
+    df = pd.merge(df_atlantic, df_cbdc, 
+            left_on="Country", right_on="Country",
+            suffixes=["","-CBDC"],
+            how = "left"
+            )
+    '''
+
+    data_dict["merged"] = df.to_dict("records")
+    return data_dict
+    
+
+
+def export_data(data_dict):
+    """Export data records from pd.DataFrame to excel.
+    """
+    recs = []
     for item in data_dict["merged"]:
 
-        # additional tables (may not be useful)
-        tag = [tag for tag in data_dict["tags"] if tag["name"] == item["tag"]][0]
-        content = data_dict["history"]["content"][0]
-        step1 = [hist for hist in content["tags"] 
-                    if (hist["tag"]["name"]==item["tag"])
-                    ]
-        changes = step1[0]["changes"] if len(step1)>0 else []
-        change_list = [chg for chg in changes if chg["property"]=="status"]
-
         # merged table
-        country = item["country"] if "country" in item.keys() else np.nan
-        status = item["mod_status"] if "mod_status" in item.keys() else np.nan
-        bank = item["centralBank"] if "centralBank" in item.keys() else np.nan
-        currency = tag["currency"] if "currency" in tag.keys() else np.nan
-        purpose = item["type"] if "type" in item.keys() else np.nan
-        partner = item["technology"] if "technology" in item.keys() else np.nan
-        software = item["Underlying technology"] if "Underlying technology" in item.keys() else np.nan
-        dlt = item["dlt"] if "dlt" in item.keys() else np.nan
+        country = item["Country"] if "Country" in item.keys() else np.nan
+        status = item["Status"] if "Status" in item.keys() else np.nan
+        bank = item["Central Bank"] if "Central Bank" in item.keys() else np.nan
+        currency = item["Digital Currency Name"] if "Digital Currency Name" in item.keys() else np.nan          
+        purpose = item["Purpose"] if "Purpose" in item.keys() else np.nan                                       
+        partner = item["Technology provider"] if "Technology provider" in item.keys() else np.nan
+        software = item["Software"] if "Software" in item.keys() else np.nan                                    
+        dlt = item["Ledger Type"] if "Ledger Type" in item.keys() else np.nan                                                  
+        permission = item["Permission"] if "Permission" in item.keys() else np.nan
+
         tech = item["goals"] if "goals" in item.keys() else np.nan
         summary = item["description"] if "description" in item.keys() else np.nan
         status_change = item["Changed Status"]
-        status_last_qtr = item["Previous Status"] if status_change == "Yes" else np.nan
+        status_last_qtr = item["Last Qtr Status"] if status_change == "Yes" else np.nan
         
         # append record
         rec = country_record(
@@ -184,8 +252,8 @@ def process_data(data_dict):
             StatusLastQtr = status_last_qtr,
 
             CentralBank = bank,
-            NationalBankPresence = '',
-            BankNames = '',
+            NationalBankPresence = '<edw_query>',
+            BankNames = '<edw_query>',
 
             CurrencyName = currency,
             Purpose = purpose,
@@ -193,9 +261,9 @@ def process_data(data_dict):
             Software = software,
             LedgerType = dlt,
 
-            BlockChainPermissions = '',
-            Technology = tech,
-            Summary = summary,
+            BlockChainPermissions = permission,
+            Technology = '<manual>',
+            Summary = '<manual>',
         )
         recs.append(rec)
     return recs
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -14,8 +14,9 @@ import pandas as pd
 from cbdc_scraper.utils import (
     get_data_cbdc, 
     get_data_atlantic,
-    check_dataframe,
-    process_data
+    check_dataframes,
+    process_data,
+    export_data
 )
 
 
@@ -33,19 +34,24 @@ def test_get_data_atlantic():
     assert [*data.keys()] == ['Atlantic']
 
 
-def test_check_dataframe():
+def test_check_dataframes():
+    data_dict = {}
     df = pd.DataFrame()
-    check = check_dataframe(df)
-    assert check == False
+    data_dict['df'] = df
+    check = check_dataframes(data_dict)
+    assert check == [False]
 
 
 def test_process_data():
+    """
+    expected output: 204 recs
+    """
     data_dict = {}
     data_dict_cbdc = get_data_cbdc()
     data_dict_atlantic = get_data_atlantic()
     data_dict.update(data_dict_cbdc)
     data_dict.update(data_dict_atlantic)
+    data_dict = process_data(data_dict)
+    recs = export_data(data_dict)
 
-    recs = process_data(data_dict)
-
-    assert len(recs) == 87
+    assert len(recs) == 236
